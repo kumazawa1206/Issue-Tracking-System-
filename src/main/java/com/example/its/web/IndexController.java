6142a9ab0c0c549c4ba822a70c6f6bb4cbe13fb0
yu ji <leon5617@gmail.com>
2023-10-24 06:12:01 +0900

    テンプレートエンジンの導入

--- src/main/java/com/example/its/web/IndexController.java
+++ src/main/java/com/example/its/web/IndexController.java
@@ -2,16 +2,14 @@ package com.example.its.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
   // GET /
   @GetMapping("/")
-  @ResponseBody
   public String index() {
-    return "<h1>Hello, World!!</h1>";
+    return "index";
   }
 
 }
