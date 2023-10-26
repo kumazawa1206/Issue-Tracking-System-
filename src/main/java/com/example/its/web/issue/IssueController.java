ea8b34cbd550a753452e0fa5e644f147247c9fe9
yu ji <leon5617@gmail.com>
2023-10-27 06:50:24 +0900

    ModelとControllerの作成

---
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -0,0 +1,23 @@
+package com.example.its.web.issue;
+
+import com.example.its.domain.issue.IssueEntity;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IssueController {
+
+  // GET /issues
+  @GetMapping("/issues")
+  public String showList(Model model) {
+    var issueList = List.of(
+        new IssueEntity(1, "概要1", "説明1"),
+        new IssueEntity(2, "概要2", "説明2"),
+        new IssueEntity(3, "概要3", "説明3")
+    );
+    model.addAttribute("issueList", issueList);
+    return "issues/List";
+  }
+}
